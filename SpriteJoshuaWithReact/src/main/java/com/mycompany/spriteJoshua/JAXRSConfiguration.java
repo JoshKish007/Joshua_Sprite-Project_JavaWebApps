c5878e1001a94f80ea1db707b1a07bed43b546fe
JoshKish007 <mr.jak3105@gmail.com>
2024-01-11 14:46:26 -0500

    Initial commit

---
+++ SpriteJoshuaWithReact/src/main/java/com/mycompany/spriteJoshua/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.spriteJoshua;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Joshua
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
